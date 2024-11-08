3c26509804838dc21ec3222c07f5384541c32905
chip <a917kk@mail.ru>
2024-11-08 18:05:06 +0300

    add nginx.conf

--- keygen/intermediate.sh
+++ keygen/intermediate.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
 # Шаг 2: Создание промежуточного сертификата и его подпись корневым CA
 # Создайте приватный ключ для промежуточного сертификата:
 # openssl genpkey -algorithm RSA -out intermediate.key -aes256
@@ -11,6 +15,6 @@
 # openssl req -config intermediate.cnf -new -keyout intermediate.key -out intermediate.csr -nodes
 
 # Создайте приватный ключ и запрос на подпись сертификата (CSR) для промежуточного сертификата:
-openssl req -config intermediate.cnf -new -keyout ../intermediate.key -out ../intermediate.csr -nodes
+openssl req -config intermediate.cnf -new -keyout ../certs/intermediate.key -out ../req/intermediate.csr -nodes
 # Создайте и подпишите промежуточный сертификат корневым CA:
-openssl x509 -req -in ../intermediate.csr -CA ../rootCA.crt -CAkey ../rootCA.key -CAcreateserial -out ../intermediate.crt -days 3650 -sha256 -extfile intermediate.cnf -extensions intermediate_req_extensions
+openssl x509 -req -in ../req/intermediate.csr -CA ../certs/rootCA.crt -CAkey ../certs/rootCA.key -CAcreateserial -out ../certs/intermediate.crt -days 3650 -sha256 -extfile intermediate.cnf -extensions intermediate_req_extensions
